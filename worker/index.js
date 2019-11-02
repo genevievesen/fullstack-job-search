f8ba964d6dd7a5556f402ed38dad5cec6fffe708
grs6333 <genevievesen23@gmail.com>
2019-11-02 16:09:16 -0400

    update cronjob to fire very hour

--- worker/index.js
+++ worker/index.js
@@ -3,4 +3,4 @@ var CronJob = require('cron').CronJob;
 const fetchGithub = require('./tasks/fetch-github')
 
 
-new CronJob('*/1 * * * *', fetchGithub, null, true, 'America/Los_Angeles'); 
+new CronJob('0 * * * *', fetchGithub, null, true, 'America/Los_Angeles'); 
